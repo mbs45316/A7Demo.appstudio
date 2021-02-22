a2d7f6fc22ec9eba1d46530a17f605b1246c7fa3
Maxwell Schutze <mbs45316@creighton.edu>
2021-02-22 15:56:28 -0600

    Update Form1.js

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -1,14 +1,12 @@
 // this is some code to test GHD 
 // and Repository process
 
-/*
-let myName = "Cindy"
+
+let myName = "Maxwell"
 console.log(`Hello, my name is ${myName}.`)
 console.log(`Welcome to Monday Demos`)
-*/
 
-/*
+
 let dogName= ""
 dogName = prompt("Enter the name of your favorite dog: ")
 console.log(`Welcome ${dogName}, it is good to have you here.`)
-*/
